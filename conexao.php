5d5d3fcf408ab2e51fbaf779cb9633d34e897347
Clicia Natalia <cliciaaraujo@outlook.com>
2020-06-11 19:38:30 -0300

    commit

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost"; //padrao server local
+    $database = "thechampion"; //nome do banco
+    $username="root";
+    $password = "";
+
+    //creando conexao
+        $conexao = mysqli_connect($servername,$username,$password,$database});
+
+?>
--- votar_lutador.php
+++ votar_lutador.php
@@ -1,11 +1,14 @@
 <?php
+include 'conexao.php';
 //cadastrar dados do form - post
 
 $nome = $_POST['nome']; //recebe valor do atributo
 $nota = $_POST['nota'];
+$mes = $_POST['mes'];
 $file = $_POST['file'];
 
-echo $nome;
+echo $sql = "INSERT INTO `lutadores`( `nome`, `nota`, `mes`) VALUES ('$nome',$nota,'$mes')";
+
 ?>
 
         
